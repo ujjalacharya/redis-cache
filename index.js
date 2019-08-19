f16435e0f355b9b6d525bd928dddcdc90e0e9961
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-08-19 21:10:16 +0545

    hit github api to send number of public repos

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
+const getRepos = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const resp = await axios.get(`https://api.github.com/users/${username}`);
+
+    return res.json(resp.data.public_repos);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Error");
+  }
+};
+
+app.get("/repos/:username", getRepos);
+
 app.listen(PORT, () => {
   console.log(`App started at port ${PORT}`);
 });
