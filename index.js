fcbadb420f5d826ab10555a34189fcbf9a45f497
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-08-20 21:25:16 +0545

    implement cache middleware

--- index.js
+++ index.js
@@ -9,20 +9,46 @@ const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
+const setResponse = (username, repos) => {
+  return `<h2>${username} has ${repos} repositories...</h2>`;
+};
+
 const getRepos = async (req, res) => {
   try {
+    console.log("Fetching data");
+
     const { username } = req.params;
 
     const resp = await axios.get(`https://api.github.com/users/${username}`);
 
-    return res.json(resp.data.public_repos);
+    const repos = resp.data.public_repos;
+
+    // Set data to Redis
+    client.setex(username, 3600, repos);
+
+    res.send(setResponse(username, repos));
   } catch (err) {
     console.error(err);
     return res.status(500).send("Error");
   }
 };
 
-app.get("/repos/:username", getRepos);
+// Cache middleware
+const cache = (req, res, next) => {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  });
+};
+
+app.get("/repos/:username", cache, getRepos);
 
 app.listen(PORT, () => {
   console.log(`App started at port ${PORT}`);
